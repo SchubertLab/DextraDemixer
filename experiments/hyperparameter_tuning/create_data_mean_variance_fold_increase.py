d13eb69194ce0b21c1c273dc5d8d0447a6b6dc83
Yang <yang.an@outlook.com>
2025-01-31 18:29:23 +0100

    Debug: remove loop to create simulation file

--- experiments/hyperparameter_tuning/create_data_mean_variance_fold_increase.py
+++ experiments/hyperparameter_tuning/create_data_mean_variance_fold_increase.py
@@ -15,24 +15,24 @@ def main():
     cov = bool(sys.argv[7])
     mean_inc = float(sys.argv[8])
     var_inc = float(sys.argv[9])
-    n = int(sys.argv[10])
+    i = int(sys.argv[10])
 
     plt.ioff()
     sim = DextramerSimulator()
-    for i in range(0, n):
-        mdata1, axs1 = sim.simulate_pmhc_data_from_distribution(total_cells=total_cell,
-                                                                nof_clones=nclones,
-                                                                p_binding_outlier=p_outlier,
-                                                                binding_ratio=p,
-                                                                binding_fold_increase_range=[mean_inc],
-                                                                variance_fold_increase_range=[var_inc],
-                                                                simulate_neg_control=True,
-                                                                use_clonotype_cov=cov,
-                                                                plot_data=True,
-                                                                rng_key=i)
-        mdata1.write(output_h5mu)
-        axs1[0, 0].get_figure().savefig(output_pdf)
-        plt.close()
+
+    mdata1, axs1 = sim.simulate_pmhc_data_from_distribution(total_cells=total_cell,
+                                                            nof_clones=nclones,
+                                                            p_binding_outlier=p_outlier,
+                                                            binding_ratio=p,
+                                                            binding_fold_increase_range=[mean_inc],
+                                                            variance_fold_increase_range=[var_inc],
+                                                            simulate_neg_control=True,
+                                                            use_clonotype_cov=cov,
+                                                            plot_data=True,
+                                                            rng_key=i)
+    mdata1.write(output_h5mu)
+    axs1[0, 0].get_figure().savefig(output_pdf)
+    plt.close()
 
 
 if __name__ == "__main__":
