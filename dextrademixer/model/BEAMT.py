f3470e7e306e4b8ce2db5094f39f736292a78f5c
benjamin.schubert <benni.schubert@gmail.com>
2025-08-01 17:01:18 +0200

    - implemented plotting function for BEAMT

--- dextrademixer/model/BEAMT.py
+++ dextrademixer/model/BEAMT.py
@@ -2,7 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Tuple
 
+import os
+
 import mudata as md
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import f1_score
 
 import jax.lax
 import jax
@@ -28,6 +33,7 @@ class BEAMT(ApMHCDeconvolution):
         super().__init__()
         self.params = None
         self.p = None
+        self.data = None
 
     def preprocess_model_data(self, mdata: md.MuData, pmhc_key: str, gex_key: str = "gex", neg_ctrl_key: str = None,
                               ir_key: str = "airr", ir_clone_key: str = None, ir_cov_key: str = None, **kwargs):
@@ -41,6 +47,10 @@ class BEAMT(ApMHCDeconvolution):
         x_neg = gex[:, neg_ctrl_key].X.toarray().reshape((N,))
 
         self._check_parameters(x, x_neg, None, None)
+
+        self.data = {"x": x,
+                     "x_neg": x_neg
+                     }
 
         self.params = {"alpha": x+1, "beta": x_neg+3}
 
@@ -75,3 +85,63 @@ class BEAMT(ApMHCDeconvolution):
         # posterior probability of belonging to the binding class
         assignment = self._predict_posterior_class(self.p, threshold, target_fdr)
         return self.p.__array__(), assignment.__array__()
+
+    def plot_results(self, assignment, p_pred, y_true=None, seed=42, config=''):
+
+        if self.p is None:
+            raise RuntimeError("Model has not been fit yet. Please call `fit` or `fit_svi` first.")
+
+        plt.figure(figsize=(6, 12))
+
+        # Plot data colored in predicted class assignment
+        plt.subplot(3, 2, 1)
+        sns.histplot(x=self.data["x"], hue=assignment,
+                     discrete=True, element="step", alpha=0.7)
+        sns.despine()
+        plt.title("Predicted class assignment")
+
+        plt.subplot(3, 2, 3)
+        sns.histplot(x=self.data["x"], hue=assignment,
+                     discrete=True, element="step", alpha=0.7)
+        sns.despine()
+        plt.yscale("log")
+        plt.title("Predicted class assignment log-scale")
+
+        plt.subplot(3, 2, 5)
+        sns.scatterplot(x=self.data["x"], y=p_pred, hue=assignment,
+                        markers={0: ".", 1: "X"})
+        sns.despine()
+        plt.ylabel("Posterior probability")
+        plt.title("Predicted probability and label of UMI count")
+
+        # Plot data colored in true class assignment
+        plt.subplot(3, 2, 2)
+        sns.histplot(x=self.data["x"], hue=y_true,
+                     discrete=True, element="step", alpha=0.7)
+        sns.despine()
+        plt.title("True class assignment")
+
+        plt.subplot(3, 2, 4)
+        sns.histplot(x=self.data["x"], hue=y_true,
+                     discrete=True, element="step", alpha=0.7)
+        sns.despine()
+        plt.yscale("log")
+        plt.title("True class assignment log-scale")
+
+        plt.subplot(3, 2, 6)
+        sns.scatterplot(x=self.data["x"], y=p_pred, hue=y_true)
+        sns.despine()
+        plt.ylabel("Posterior probability")
+        plt.title("Predicted probability of UMI count with true label")
+
+        # Save plot
+        try:
+            f1 = f1_score(assignment, y_true)
+        except:
+            # if y_true is None or str
+            f1 = -1
+        plt.suptitle(config.replace("_", " ").replace("ncell", "\nncell") + f"\nF1-score {f1:.3f}",)
+        os.makedirs("figs", exist_ok=True)
+        plt.savefig(f"figs/{config}.png")
+        plt.show()
+        plt.close()
