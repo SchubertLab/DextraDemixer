d1596f65166d69a529c082d8e6a9fb917944e340
Yang <yang.an@outlook.com>
2025-09-18 15:51:41 +0200

    feature: sample negative and noise parameters from fitted distribution for simulation

--- experiments/synthetic_benchmark/create_data_mean_variance_fold_increase.py
+++ experiments/synthetic_benchmark/create_data_mean_variance_fold_increase.py
@@ -3,6 +3,7 @@ sys.path.append("../../")
 
 import matplotlib.pyplot as plt
 from dextrademixer.utils import DextramerSimulator
+import hashlib
 
 
 def main():
@@ -12,25 +13,31 @@ def main():
     nclones = int(sys.argv[4])
     p_outlier = float(sys.argv[5])
     p = float(sys.argv[6])
-    cov = bool(sys.argv[7])
+    cov = bool(int(sys.argv[7]))
     mean_inc = float(sys.argv[8])
     var_inc = float(sys.argv[9])
-    n = int(sys.argv[10])
+    i = int(sys.argv[10])
+
+    sim_config = f'{total_cell}_{nclones}_{p_outlier}_{p}_{cov}_{mean_inc}_{var_inc}_{i}'
+    # Use sim_hash to ensure reproducibility, but have variance in seed
+    sim_hash = hashlib.sha256(sim_config.encode('utf-8')).digest()
+    seed = int.from_bytes(sim_hash[:4], byteorder='little')
 
     plt.ioff()
     sim = DextramerSimulator()
-    mdata1, axs1 = sim.simulate_pmhc_data_from_distribution(total_cells=total_cell,
+
+    mdata1, fig = sim.simulate_pmhc_data_from_distribution(total_cells=total_cell,
                                                             nof_clones=nclones,
                                                             p_binding_outlier=p_outlier,
                                                             binding_ratio=p,
-                                                            binding_fold_increase_range=[mean_inc],
-                                                            variance_fold_increase_range=[var_inc],
+                                                            mean_inc=mean_inc,
+                                                            var_inc=var_inc,
                                                             simulate_neg_control=True,
                                                             use_clonotype_cov=cov,
                                                             plot_data=True,
-                                                            rng_key=n)
+                                                            rng_key=seed)
     mdata1.write(output_h5mu)
-    axs1[0, 0].get_figure().savefig(output_pdf)
+    fig.savefig(output_pdf)
     plt.close()
 
 
