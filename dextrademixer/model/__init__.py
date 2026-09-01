5fc1ef34b664b29fdff827866a480ab2bce6fb7f
Yang <yang.an@outlook.com>
2026-09-01 13:56:07 +0200

    __all__

--- dextrademixer/model/__init__.py
+++ dextrademixer/model/__init__.py
@@ -3,3 +3,13 @@ from dextrademixer.model.DextraDemixer import DextraDemixer
 from dextrademixer.model.DextraDemixerMulti import DextraDemixerMulti
 from dextrademixer.model.BEAM import BEAM
 from dextrademixer.model.ICON import ICON
+from dextrademixer.model.ITRAP import ITRAP
+
+__all__ = [
+    "ApMHCDeconvolution",
+    "DextraDemixer",
+    "DextraDemixerMulti",
+    "BEAM",
+    "ICON",
+    "ITRAP",
+]
